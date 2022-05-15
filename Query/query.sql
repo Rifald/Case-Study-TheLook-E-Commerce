12e60b8a5a0da07dd4913883b044dc884d424b3f
rifaldiqbal <43335005+Rifald@users.noreply.github.com>
2022-05-15 17:18:47 +0700

    Update query.sql

--- Query/query.sql
+++ Query/query.sql
@@ -1,8 +1,9 @@
-SELECT o.order_id, o.status, 
-gender,
-o.created_at,
-oi.sale_price,
-p.category
+SELECT o.order_id, 
+       o.status, 
+       gender,
+       o.created_at,
+       oi.sale_price,
+       p.category
 FROM `bigquery-public-data.thelook_ecommerce.orders` o 
-LEFT JOIN `bigquery-public-data.thelook_ecommerce.order_items` oi ON o.order_id = oi.order_id
-LEFT JOIN `bigquery-public-data.thelook_ecommerce.products` p ON oi.product_id = p.id;
+      LEFT JOIN `bigquery-public-data.thelook_ecommerce.order_items` oi ON o.order_id = oi.order_id
+      LEFT JOIN `bigquery-public-data.thelook_ecommerce.products` p ON oi.product_id = p.id;
